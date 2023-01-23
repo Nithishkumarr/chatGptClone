2b4c961a8ec62455ee0183ba407569a8819f819c
Nithishkumarr <nithishrajesh18me@nieit.ac.in>
2023-01-23 18:21:48 +0530

    initial UI design

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <aside className="sidemenu">
+        <div className="side-menu-button">
+          <span>+</span>NewChat
+        </div>
+      </aside>
+      <section className="chatbot">
+        <div className="chat-log">
+          <div className="chat-message">
+            <div className="chat-message-centre">
+              <div className="avatar"></div>
+              <div className="message">hello there</div>
+            </div>
+
+            <div className="chatgpt">
+              <div className="chat-message-centre">
+                <div className="avatar"></div>
+                <div className="message">Iam an AI</div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="chat-input-holder">
+          <textarea rows="1" className="chat-input-textarea"></textarea>
+        </div>
+      </section>
     </div>
   );
 }
